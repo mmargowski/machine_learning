ea1d608400140408bce4166ee02676724052f09b
Marius Margowski <mariusmargowski89@gmail.com>
2019-02-22 07:47:23 +0100

    Added Simple Linear Regression

--- Data Preprocessing/data_preprocessing_template.py
+++ Data Preprocessing/data_preprocessing_template.py
@@ -30,7 +30,7 @@ Y = labelencoder_Y.fit_transform(Y)
 
 # Splitting the dataset
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size = 0.2)
 
 '''
 # Feature Scaling
